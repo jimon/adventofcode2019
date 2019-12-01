81a98c3bf181d25b92d9a82b1bfdedfb67bed448
Dmytro Ivanov <dmytro@beardsvibe.com>
2019-12-01 11:08:26 +0100

    day01

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace day01
 {
     class Program
     {
+        static int FuelRequirementSimple(int moduleMass)
+        {
+            return (moduleMass / 3) - 2;
+        }
+
+        static int FuelRequirementExtra(int moduleMass)
+        {
+            int totalFuel = 0;
+            int extraFuel = FuelRequirementSimple(moduleMass);
+            
+            while (extraFuel > 0)
+            {
+                totalFuel += extraFuel;
+                extraFuel = FuelRequirementSimple(extraFuel);
+            }
+
+            return totalFuel;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var allModuleMasses = File.ReadAllLines("input1.txt").Select(l => Int32.Parse(l));
+            
+            Console.WriteLine($"fuel simple = {allModuleMasses.Select(m => FuelRequirementSimple(m)).Sum()}");
+            Console.WriteLine($"fuel extra = {allModuleMasses.Select(m => FuelRequirementExtra(m)).Sum()}");
         }
     }
 }
