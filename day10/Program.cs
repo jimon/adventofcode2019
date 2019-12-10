114e870202ceebe3d1d70cd71315119cafb91848
Dmytro Ivanov <dmytro@beardsvibe.com>
2019-12-10 20:02:19 +0100

    day10 part2

--- day10/Program.cs
+++ day10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -12,7 +13,7 @@ namespace day10
             return Math.Abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1) /
                    Math.Sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
         }
-        static int CountVisible(char[][] field, int ax, int ay, int w, int h, double deltaAngle, double maxRadius)
+        static int CountVisible(char[][] field, int ax, int ay, int w, int h)
         {
             int count = 0;
             
@@ -59,42 +60,118 @@ namespace day10
             return count;
         }
 
-        static int FindBestPoint(string filename)
+        static (int count, int x, int y) FindBestPoint(char[][] field)
         {
-            var field = File.ReadAllLines(filename).Select(x => x.ToArray()).ToArray();
-            int max = 0;
+            int maxCount = 0;
+            int maxX = 0;
+            int maxY = 0;
             int w = field[0].Length;
             int h = field.Length;
-            double deltaAngle = Math.Min(Math.Atan2(1.0, (double)w), Math.Atan2((double)h, 1.0));
-            double maxRadius = Math.Sqrt((double) (w * w + h * h));
             for (int y = 0; y < h; ++y)
             {
                 for (int x = 0; x < w; ++x)
                 {
                     if (field[y][x] == '#')
                     {
-                        var temp = CountVisible(field, x, y, w, h, deltaAngle, maxRadius);
-//                        Console.Write($"{temp}");
-                    
-                        if (temp > max)
-                            max = temp;
+                        var temp = CountVisible(field, x, y, w, h);
+                        if (temp > maxCount)
+                        {
+                            maxCount = temp;
+                            maxX = x;
+                            maxY = y;
+                        }
                     }
-//                    else
-//                        Console.Write(".");
                 }
-//                Console.Write("\n");
             }
-            return max;
+            return (count: maxCount, x: maxX, y: maxY);
+        }
+
+        static (int x, int y) FindDestroyedAsteroid(char[][] field, int ax, int ay, int number)
+        {
+            int w = field[0].Length;
+            int h = field.Length;
+
+            double maxRadius = Math.Sqrt((double) (w * w + h * h));
+
+            int count = 0;
+            
+            // iterate through all map points and sort angles for iteration
+            var validAnglesList = new List<double>();
+            for (int y = 0; y < h; ++y)
+                for (int x = 0; x < w; ++x)
+                    if (x != ax || y != ay)
+                        validAnglesList.Add(-Math.Atan2(x - ax, y - ay) + Math.PI / 2.0);
+
+            var validAngles = validAnglesList.Distinct().OrderBy(x => x).ToArray();
+            int angleIndex = 0;
+            while (true)
+            {
+                double angle = validAngles[angleIndex];
+                double bx = Math.Cos(angle) * maxRadius + ax;
+                double by = Math.Sin(angle) * maxRadius + ay;
+
+                for (double t = 0.0; t <= 1.0; t += 1.0 / maxRadius)
+                {
+                    double pxf = (bx - ax) * t + ax;
+                    double pyf = (by - ay) * t + ay;
+                    
+                    var px = (int) Math.Round(pxf);
+                    var py = (int) Math.Round(pyf);
+                    
+                    if (px < 0 || py < 0 || px >= w || py >= h)
+                        break;
+
+                    if (ax == px && ay == py)
+                        continue;
+
+                    if (field[py][px] != '#')
+                        continue;
+                    
+                    if (PointToLineDist(px, py, ax, ay, bx, by) < 0.01)
+                    {
+                        count++;
+
+                        if (count == number)
+                            return (x: px, y: py);
+                        field[py][px] = '.';
+                        break;
+                    }
+                }
+
+                angleIndex++;
+                if (angleIndex >= validAngles.Length)
+                    angleIndex = 0;
+            }
+        }
+
+        static char[][] Parse(string filename)
+        {
+            return File.ReadAllLines(filename).Select(x => x.ToArray()).ToArray();
         }
         
         static void Main(string[] args)
         {
-            Console.WriteLine($"ref1 = {FindBestPoint("ref1.txt")} = 8");
-            Console.WriteLine($"ref2 = {FindBestPoint("ref2.txt")} = 33");
-            Console.WriteLine($"ref3 = {FindBestPoint("ref3.txt")} = 35");
-            Console.WriteLine($"ref4 = {FindBestPoint("ref4.txt")} = 41");
-            Console.WriteLine($"ref5 = {FindBestPoint("ref5.txt")} = 210");
-            Console.WriteLine($"input1 = {FindBestPoint("input1.txt")} = 282");
+            Console.WriteLine($"ref1 = {FindBestPoint(Parse("ref1.txt")).count} = 8");
+            Console.WriteLine($"ref2 = {FindBestPoint(Parse("ref2.txt")).count} = 33");
+            Console.WriteLine($"ref3 = {FindBestPoint(Parse("ref3.txt")).count} = 35");
+            Console.WriteLine($"ref4 = {FindBestPoint(Parse("ref4.txt")).count} = 41");
+            Console.WriteLine($"ref5 = {FindBestPoint(Parse("ref5.txt")).count} = 210");
+            
+            {
+                var field = Parse("ref5.txt");
+                var best = FindBestPoint(field);
+                var point = FindDestroyedAsteroid(field, best.x, best.y, 200);
+                Console.WriteLine($"ref5 part2 = {point.x},{point.y} = 8,2");
+            }
+
+            {
+                var field = Parse("input1.txt");
+                var best = FindBestPoint(field);
+                Console.WriteLine($"part1 = {best.count} = 282");
+
+                var point = FindDestroyedAsteroid(field, best.x, best.y, 200);
+                Console.WriteLine($"part2 = {point.x * 100 + point.y} = 1008");
+            }
         }
     }
 }
