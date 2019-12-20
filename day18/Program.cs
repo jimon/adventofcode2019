a635f20148ebd9d0523a2e3836cef67eab469653
Dmytro Ivanov <dmytro@unity3d.com>
2019-12-20 16:20:17 +0100

    day18 wip

--- day18/Program.cs
+++ day18/Program.cs
@@ -2,28 +2,74 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Dijkstra.NET.Graph;
-using Dijkstra.NET.ShortestPath;
 
 namespace day18
 {
+    struct Vec2
+    {
+        public int x;
+        public int y;
+
+        public static Vec2 Set(int x, int y)
+        {
+            var v = new Vec2();
+            v.x = x;
+            v.y = y;
+            return v;
+        }
+    };
+
+    class Vec2Cost : IComparable<Vec2Cost>
+    {
+        public Vec2 pos;
+        public int cost;
+
+        public Vec2Cost(Vec2 setPos, int setCost)
+        {
+            pos = setPos;
+            cost = setCost;
+        }
+
+        public int CompareTo(Vec2Cost other)
+        {
+            return cost.CompareTo(other.cost);
+        }
+    };
+    
     class Map
     {
-        private Graph<(int x, int y), int> map;
-        private char[,] cmap;
-        private Dictionary<(int x, int y), uint> posToMapIndex;
-        private Dictionary<char, (int x, int y)> keyToPos;
-        private Dictionary<char, (int x, int y)> doorToPos;
-        private HashSet<char> openDoors;
+        private char[,] map;
+        private Dictionary<char, Vec2> keyToPos = new Dictionary<char, Vec2>();
+        private Dictionary<char, Vec2> doorToPos = new Dictionary<char, Vec2>();
+        private HashSet<char> openDoors = new HashSet<char>();
         private int w;
         private int h;
 
-        public Map(char[,] setCmap, int setW, int setH)
+        public Map(char[,] setMap, int setW, int setH)
         {
-            cmap = setCmap;
+            map = setMap;
             w = setW;
             h = setH;
-            ResetDoors();
+
+            for (int x = 0; x < w; ++x)
+            {
+                for (int y = 0; y < h; ++y)
+                {
+                    var v = map[x, y];
+                    var pos = Vec2.Set(x, y);
+
+                    if ((v >= 'a' && v <= 'z') || v == '@')
+                    {
+                        keyToPos[v] = pos;
+                        map[x, y] = '.';
+                    }
+                    else if (v >= 'A' && v <= 'Z')
+                    {
+                        doorToPos[v.ToString().ToLower()[0]] = pos;
+                        map[x, y] = '.';
+                    }
+                }
+            }
         }
 
         public char[] Keys()
@@ -33,95 +79,69 @@ namespace day18
 
         public int? Distance(char fromKey, char toKey)
         {
-            var r = map.Dijkstra(posToMapIndex[keyToPos[fromKey]], posToMapIndex[keyToPos[toKey]]);
+            var visited = new bool[w, h];
+            var dst = new C5.IPriorityQueueHandle<Vec2Cost>[w, h]; 
+            var heap = new C5.IntervalHeap<Vec2Cost>();
 
-            if (r.IsFounded)
-                return r.Distance;
-            else
-                return null;
+            var start = keyToPos[fromKey];
+            for (int x = 0; x < w; ++x)
+            {
+                for (int y = 0; y < h; ++y)
+                {
+                    heap.Add(ref dst[x, y], new Vec2Cost(Vec2.Set(x, y), x == start.x && y == start.y ? 0 : Int32.MaxValue));
+                }
+            }
+
+            while (!heap.IsEmpty)
+            {
+                C5.IPriorityQueueHandle<Vec2Cost> handle = null;
+                Vec2Cost p = heap.DeleteMin(out handle);
+                visited[p.pos.x, p.pos.y] = true;
+                
+
+                foreach (var adj in AdjacentTo(p.pos))
+                {
+                    var v = map[adj.x, adj.y];
+                    if (v == '#')
+                        continue;
+
+                    var old_cost = dst[adj.x, adj.y];
+                    var new_cost = p.cost + 1;
+                    if (new_cost < old_cost)
+                    {
+                        distance[adj.x, adj.y] = new_cost;
+                        if (!visited)
+                        {
+                            heap.Replace();
+                        }
+                    }
+                }
+            }
+            
+            return null;
         }
 
         public void OpenDoor(char door)
         {
             if (!openDoors.Contains(door))
                 openDoors.Add(door);
-
-            var pos = doorToPos[door];
-            var posMapIndex = posToMapIndex[pos];
-            foreach (var adj in AdjacentTo(pos.x, pos.y, w, h))
-            {
-                if (cmap[adj.x, adj.y] == '#')
-                    continue;
-                ConnectCells(posMapIndex, adj, posToMapIndex[adj]);
-            }
         }
 
         public void ResetDoors()
         {
-            // WTF?, is there a better way to disconnect edges in this library? 
-            map = new Graph<(int x, int y), int>();
             openDoors = new HashSet<char>();
-            posToMapIndex = new Dictionary<(int x, int y), uint>();
-            keyToPos = new Dictionary<char, (int x, int y)>();
-            doorToPos = new Dictionary<char, (int x, int y)>();
-
-            for (int y = 0; y < h; ++y)
-            {
-                for (int x = 0; x < w; ++x)
-                {
-                    if (cmap[x, y] == '#')
-                        continue;
-
-                    var pos = (x: x, y: y);
-                    posToMapIndex[pos] = map.AddNode(pos);
-                }
-            }
-
-            foreach (var p in posToMapIndex)
-            {
-                var pos = (x: p.Key.x, y: p.Key.y);
-                char v = cmap[pos.x, pos.y];
-
-                if ((v >= 'a' && v <= 'z') || v == '@')
-                    keyToPos[v] = pos;
-                else if (v >= 'A' && v <= 'Z')
-                {
-                    doorToPos[v.ToString().ToLower()[0]] = pos;
-                    continue;
-                }
-
-                foreach (var adj in AdjacentTo(pos.x, pos.y, w, h))
-                {
-                    var va = cmap[adj.x, adj.y];
-                    if ((va == '#') || (va >= 'A' && va <= 'Z'))
-                        continue;
-
-                    ConnectCells(p.Value, adj, posToMapIndex[adj]);
-                }
-            }
         }
 
-        private void ConnectCells(uint posMapIndex, (int x, int y) adj, uint adjMapIndex)
+        private IEnumerable<Vec2> AdjacentTo(Vec2 p)
         {
-            var v = cmap[adj.x, adj.y];
-            if (v != '#' && ((v == '.') || (v == '@') || (v >= 'a' && v <= 'z') ||
-                             (v >= 'A' && v <= 'Z' && openDoors.Contains(v))))
-            {
-                map.Connect(posMapIndex, adjMapIndex, 1, 0);
-                map.Connect(adjMapIndex, posMapIndex, 1, 0);
-            }
-        }
-
-        private static IEnumerable<(int x, int y)> AdjacentTo(int x, int y, int w, int h)
-        {
-            if (y > 0)
-                yield return (x: x, y: y - 1);
-            if (y < h - 1)
-                yield return (x: x, y: y + 1);
-            if (x > 0)
-                yield return (x: x - 1, y: y);
-            if (x < w - 1)
-                yield return (x: x + 1, y: y);
+            if (p.y > 0)
+                yield return Vec2.Set(p.x, p.y - 1);
+            if (p.y < h - 1)
+                yield return Vec2.Set(p.x, p.y + 1);
+            if (p.x > 0)
+                yield return Vec2.Set(p.x - 1, p.y);
+            if (p.x < w - 1)
+                yield return Vec2.Set(p.x + 1, p.y);
         }
     };
 
@@ -153,15 +173,15 @@ namespace day18
 
             Console.WriteLine($"{map.Distance('@', 'a')}");
 
-            map.OpenDoor('c');
-            map.OpenDoor('a');
-
-            Console.WriteLine($"{map.Distance('@', 'b')}");
-            Console.WriteLine($"{map.Distance('@', 'e')}");
-
-            map.ResetDoors();
-
-            Console.WriteLine($"{map.Distance('@', 'b')}");
+            // map.OpenDoor('c');
+            // map.OpenDoor('a');
+            //
+            // Console.WriteLine($"{map.Distance('@', 'b')}");
+            // Console.WriteLine($"{map.Distance('@', 'e')}");
+            //
+            // map.ResetDoors();
+            //
+            // Console.WriteLine($"{map.Distance('@', 'b')}");
         }
     }
 }
