dbb4b43f34b6307eaa1e68b9e165ef874d7f06ce
Dmytro Ivanov <dmytro@beardsvibe.com>
2019-12-21 11:37:53 +0100

    day18 wip

--- day18/Program.cs
+++ day18/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using C5;
 
 namespace day18
 {
@@ -36,9 +38,37 @@ namespace day18
         }
     };
 
+    class CacheKey : IStructuralEquatable
+    {
+        public char from;
+        public bool[] state;
+
+        public bool Equals(object? other, IEqualityComparer comparer)
+        {
+            var k = (CacheKey) other;
+            if (from == k.from && state.Length == k.state.Length)
+            {
+                for (int i = 0; i < state.Length; ++i)
+                    if (state[i] != k.state[i])
+                        return false;
+                return true;
+            }
+
+            return false;
+        }
+
+        public int GetHashCode(IEqualityComparer comparer)
+        {
+            int hash = from.GetHashCode();
+            for (int i = 0; i < state.Length; ++i)
+                hash = hash * 23 + state[i].GetHashCode();
+            return hash;
+        }
+    };
+
     class Map
     {
-        private char[,] map;
+        private Byte[,] map;
         public int keysCount;
         private Dictionary<char, Vec2> keyToPos = new Dictionary<char, Vec2>();
         private Dictionary<char, Vec2> doorToPos = new Dictionary<char, Vec2>();
@@ -47,40 +77,77 @@ namespace day18
 
         public Map(char[,] setMap, int setW, int setH)
         {
-            map = setMap;
             w = setW;
             h = setH;
+            map = new byte[w,h];
 
             for (int x = 0; x < w; ++x)
             {
                 for (int y = 0; y < h; ++y)
                 {
-                    var v = map[x, y];
+                    var v = setMap[x, y];
+                    
                     var pos = Vec2.Set(x, y);
 
+                    if (v == '#')
+                    {
+                        map[x, y] = 0;
+                    }
+                    else if (v == '.')
+                    {
+                        map[x, y] = byte.MaxValue;
+                    }
                     if ((v >= 'a' && v <= 'z') || v == '@')
                     {
                         keysCount++;
                         keyToPos[v] = pos;
-                        map[x, y] = '.';
+                        map[x, y] = byte.MaxValue;
                     }
                     else if (v >= 'A' && v <= 'Z')
                     {
                         var vl = v.ToString().ToLower()[0];
                         doorToPos[vl] = pos;
-                        map[x, y] = vl;
+                        map[x, y] = (byte)(vl - 'a' + 1);
                     }
                 }
             }
         }
 
+        private bool[,] visited;
+        private IPriorityQueueHandle<Vec2Cost>[,] heapHandles;
+        private int[,] distance;
+        private bool[,] reachable;
+        private IntervalHeap<Vec2Cost> heap;
+
+        public Dictionary<CacheKey, (char key, int dist)[]> cache = new Dictionary<CacheKey, (char key, int dist)[]>();
+
         public (char key, int dist)[] Distance(char fromKey, bool[] reachedKeys)
         {
-            var visited = new bool[w, h];
-            var heapHandles = new C5.IPriorityQueueHandle<Vec2Cost>[w, h];
-            var distance = new int[w, h];
-            var reachable = new bool[w, h];
-            var heap = new C5.IntervalHeap<Vec2Cost>();
+            CacheKey cacheKey = new CacheKey();
+            cacheKey.from = fromKey;
+            cacheKey.state = (bool[])reachedKeys.Clone();
+            
+            if (cache.ContainsKey(cacheKey))
+            {
+                 return cache[cacheKey];
+            }
+            
+            if (visited == null)
+            {
+                visited = new bool[w, h];
+                heapHandles = new C5.IPriorityQueueHandle<Vec2Cost>[w, h];
+                distance = new int[w, h];
+                reachable = new bool[w, h];
+                heap = new C5.IntervalHeap<Vec2Cost>();
+
+                for (int x = 0; x < w; ++x)
+                {
+                    for (int y = 0; y < h; ++y)
+                    {
+                        heap.Add(ref heapHandles[x, y], new Vec2Cost(Vec2.Set(x, y), Int32.MaxValue));
+                    }
+                }
+            }
 
             var start = keyToPos[fromKey];
 
@@ -90,24 +157,27 @@ namespace day18
                 {
                     var v = map[x, y];
                     var d = (x == start.x && y == start.y) ? 0 : Int32.MaxValue;
+                    visited[x, y] = false;
                     distance[x, y] = d;
-                    if ((v != '#') && ((v == '.') || reachedKeys[v]))
+                    if ((v > 0) && ((v == byte.MaxValue) || reachedKeys[v - 1]))
                     {
                         reachable[x, y] = true;
-                        heap.Add(ref heapHandles[x, y], new Vec2Cost(Vec2.Set(x, y), d));
+                        heap.Replace(heapHandles[x, y], new Vec2Cost(Vec2.Set(x, y), d));
                     }
+                    else
+                        reachable[x, y] = false;
                 }
             }
 
             while (!heap.IsEmpty)
             {
                 C5.IPriorityQueueHandle<Vec2Cost> handle = null;
-                Vec2Cost p = heap.DeleteMin(out handle);
+                Vec2Cost p = heap.FindMin(out handle);
                 // some nodes are not actually reachable
                 if (p.cost == Int32.MaxValue)
                     break;
+                heap.Replace(heapHandles[p.pos.x, p.pos.y], new Vec2Cost(p.pos, Int32.MaxValue));
                 visited[p.pos.x, p.pos.y] = true;
-                heapHandles[p.pos.x, p.pos.y] = null;
                 foreach (var adj in AdjacentTo(p.pos))
                 {
                     if (!reachable[adj.x, adj.y])
@@ -122,20 +192,11 @@ namespace day18
                     }
                 }
             }
-            //
-            // for (int y = 0; y < h; ++y)
-            // {
-            //     for (int x = 0; x < w; ++x)
-            //     {
-            //         Console.Write((distance[x, y] < Int32.MaxValue) ? 1 : 0);
-            //     }
-            //     Console.Write('\n');
-            // }
 
             var dist = new List<(char key, int dist)>();
             foreach (var p in keyToPos)
             {
-                if (p.Key != fromKey)
+                if (p.Key != fromKey && p.Key != '@')
                 {
                     int d = distance[p.Value.x, p.Value.y];
                     if (d != Int32.MaxValue)
@@ -144,7 +205,11 @@ namespace day18
                     }
                 }
             }
-            return dist.ToArray();
+
+            var distArr = dist.ToArray();
+
+            cache[cacheKey] = distArr;
+            return distArr;
         }
 
         private IEnumerable<Vec2> AdjacentTo(Vec2 p)
@@ -179,30 +244,42 @@ namespace day18
 
         static int Part1(Map map)
         {
-            var reachedKeys = new bool[255];
-            reachedKeys['@'] = true;
+            var reachedKeys = new bool[map.keysCount];
             return Part1Rec(map, '@', reachedKeys, 1);
         }
 
+        static long progress = 0;
+
         static int Part1Rec(Map map, char key, bool[] reachedKeys, int reachedKeysCount)
         {
+            progress++;
+            if (progress % 100000 == 0)
+            {
+                Console.WriteLine($"{progress}");
+            }
+
+            if (reachedKeysCount >= map.keysCount)
+                return 0;
+
             var dist = map
                 .Distance(key, reachedKeys)
-                .Where(x => !reachedKeys[x.key])
+                .Where(x => !reachedKeys[x.key - 'a'])
                 .OrderBy(x => x.dist);
 
             int best = Int32.MaxValue;
 
             foreach (var p in dist)
             {
-                reachedKeys[p.key] = true;
+                if (p.dist > best)
+                    continue;
+                reachedKeys[p.key - 'a'] = true;
                 int val = Part1Rec(map, p.key, reachedKeys, reachedKeysCount + 1);
-                reachedKeys[p.key] = false;
+                reachedKeys[p.key - 'a'] = false;
                 if (val < Int32.MaxValue && (val + p.dist < best))
                     best = val + p.dist;
             }
 
-            return best < Int32.MaxValue ? best : (reachedKeysCount == map.keysCount ? 0 : Int32.MaxValue);
+            return best;
         }
 
         static void Main(string[] args)
@@ -211,6 +288,8 @@ namespace day18
             Console.WriteLine($"ref2 = {Part1(Parse("ref2.txt"))} = 132");
             Console.WriteLine($"ref3 = {Part1(Parse("ref3.txt"))} = 136");
             Console.WriteLine($"ref4 = {Part1(Parse("ref4.txt"))} = 81");
+            
+            Console.WriteLine($"part1 = {Part1(Parse("input1.txt"))} = ");
         }
     }
 }
