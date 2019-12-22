008890e5decf05ba6d62878b8372bf73aae61ee5
Dmytro Ivanov <dmytro@beardsvibe.com>
2019-12-22 17:49:21 +0100

    day22 part1 rework

--- day22/Program.cs
+++ day22/Program.cs
@@ -2,90 +2,54 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 
 namespace day22
 {
-    enum OpType
-    {
-        Cut,
-        Deal,
-        Inverse
-    };
-
-    class Op
-    {
-        public OpType type;
-        public int offset;
-
-        public Op(OpType setType, int setOffset = 0)
-        {
-            type = setType;
-            offset = setOffset;
-        }
-    };
-
     class Program
     {
-        static IEnumerable<Op> ParseEnum(string filename)
+        // convert operations to a*x + b ≡ y (mod w)
+        static (BigInteger a, BigInteger b) Parse(string filename, BigInteger deckSize)
         {
+            BigInteger a = new BigInteger(1);
+            BigInteger b = new BigInteger(0);
+
             foreach (var l in File.ReadAllLines(filename))
             {
                 if (l == "deal into new stack")
-                    yield return new Op(OpType.Inverse);
+                {
+                    a *= -1;
+                    b *= -1;
+                    b += deckSize - 1;
+                }
                 else
                 {
                     int o = Int32.Parse(l.Split(" ").Last());
+
                     if (l.StartsWith("cut "))
-                        yield return new Op(OpType.Cut, o);
+                    {
+                        b += deckSize - o;
+                    }
                     else if (l.StartsWith("deal with increment "))
-                        yield return new Op(OpType.Deal, o);
+                    {
+                        a *= o;
+                        b *= o;
+                    }
                 }
-            }
-        }
 
-        static Op[] Parse(string filename)
-        {
-            return ParseEnum(filename).ToArray();
-        }
-
-        static int[] ShuffleDeck(int[] deck, Op[] ops)
-        {
-            foreach (var op in ops)
-            {
-                switch (op.type)
-                {
-                    case OpType.Cut:
-                        if (op.offset > 0)
-                            deck = deck.Skip(op.offset).Concat(deck.Take(op.offset)).ToArray();
-                        else
-                            deck = deck.TakeLast(-op.offset).Concat(deck.SkipLast(-op.offset)).ToArray();
-                        break;
-                    case OpType.Deal:
-                        var t = new int[deck.Length];
-                        for (int i = 0; i < deck.Length; ++i)
-                            t[i] = Int32.MinValue;
-                        for (int i = 0; i < deck.Length; ++i)
-                        {
-                            var j = (i * op.offset) % deck.Length;
-                            if (t[j] != Int32.MinValue)
-                                throw new ArgumentException("hm?");
-                            t[j] = deck[i];
-                        }
-
-                        deck = t;
-                        break;
-                    case OpType.Inverse:
-                        deck = deck.Reverse().ToArray();
-                        break;
-                }
+                a %= deckSize;
+                b %= deckSize;
             }
 
+            return (a, b);
+            
+        }
+        static int[] ShuffleDeck(int deckSize, BigInteger a, BigInteger b)
+        {
+            var deck = new int[deckSize];
+            for (int i = 0; i < deckSize; ++i)
+                deck[(int)SolveDiophantine(a, i, b, deckSize)] = i;
             return deck;
-        }
-
-        static int[] Deck(int count)
-        {
-            return Enumerable.Range(0, count).ToArray();
         }
 
         static string DeckToString(int[] deck)
@@ -95,17 +59,39 @@ namespace day22
 
         static void RefPart1(string filename, string refStr)
         {
+            var deckSize = 10;
+            var (a, b) = Parse(filename, deckSize);
+            var deck = ShuffleDeck(deckSize, a, b);
+            var refDeck = refStr.Split(" ").Select(x => Int32.Parse(x)).ToArray();
             Console.WriteLine(
-                $"{filename} = {DeckToString(ShuffleDeck(Deck(10), Parse(filename)))} = {refStr}");
+                $"{filename} = {DeckToString(deck)} = {refStr} ({(deck.SequenceEqual(refDeck) ? "ok" : "not ok")})");
         }
 
         static int Part1()
         {
-            var deck = ShuffleDeck(Deck(10007), Parse("input1.txt"));
-            for(int i = 0; i < deck.Length; ++i)
-                if (deck[i] == 2019)
-                    return i;
-            return Int32.MinValue;
+            var deckSize = 10007;
+            var (a, b) = Parse("input1.txt", deckSize);
+            return (int)SolveDiophantine(a, 2019, b, deckSize);
+        }
+
+        // a*x + b ≡ y (mod w)
+        // given a, x, b, w, find smallest positive y
+        static BigInteger SolveDiophantine(BigInteger a, BigInteger x, BigInteger b, BigInteger w)
+        {
+            int k = 0;
+
+            // lol bruteforce
+            while (true)
+            {
+                var y = a * x + b + w * k;
+
+                if (y < 0)
+                    k++;
+                else if (y >= w)
+                    k--;
+                else
+                    return y;
+            }
         }
 
         static void Main(string[] args)
@@ -114,8 +100,15 @@ namespace day22
             RefPart1("ref2.txt", "3 0 7 4 1 8 5 2 9 6");
             RefPart1("ref3.txt", "6 3 0 7 4 1 8 5 2 9");
             RefPart1("ref4.txt", "9 2 5 8 1 4 7 0 3 6");
-            
+
             Console.WriteLine($"part1 = {Part1()} = 6129");
+
+
+
+            // part2
+            // 119315717514047 cards
+            // 101741582076661 times
+            // 2020 card value
         }
     }
 }
